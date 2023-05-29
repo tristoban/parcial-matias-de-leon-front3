74de63b9bef21624c895df24e53149998f1e2992
tristoban <hola@cloomy.us>
2023-05-29 20:40:48 -0300

    Primer commit, post paso 3 de la mesa de trabajo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Card from './components/Card'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      return <div>Hola, soy una tarjeta</div>
+    </div>
+  )
+}
+
+export default Card
